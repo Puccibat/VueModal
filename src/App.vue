51625297dd0220bb7e2362ae021f23b4d2c0ded2
baptiste.puccinelli <baptiste.puccinelli@rakuten.com>
2022-08-16 19:05:36 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <div v-if="showModalOne">
+    <ModalItem @close='toggleModalOne' >
+      <template v-slot:content>
+        <h1>This the first Modal</h1>
+        <p>1</p>
+      </template>
+    </ModalItem>
+  </div>
+  <div v-if="showModalTwo">
+    <ModalItem @close='toggleModalTwo' >
+      <template v-slot:content>
+        <h1>This the second Modal</h1>
+        <p>2</p>
+      </template>
+    </ModalItem>
+  </div>
+  <div v-if="showModalThree">
+    <ModalItem @close='toggleModalThree' :header='header' :text='text' />
+  </div>
+  <div class="btns">
+    <button @click.shift='toggleModalOne'>Open Modal 1 (hold shift & click)</button>
+    <button @click='toggleModalTwo'>Open Modal 2</button>
+    <button @click='toggleModalThree'>Open Modal 3</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalItem from './components/Modal-item.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ModalItem },
+  data() {
+    return {
+      title: 'My first Vue App !!! :)',
+      header: 'This is my first Vue modal',
+      text: 'So cool !',
+      showModalOne: false,
+      showModalTwo: false,
+      showModalThree: false,
+    }
+  },
+  methods: {
+    toggleModalOne() {
+      this.showModalOne = !this.showModalOne
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    },
+    toggleModalThree() {
+      this.showModalThree = !this.showModalThree
+    },
   }
 }
 </script>
@@ -22,5 +64,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btns {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 250px;
+  margin: auto;
+}
+
+.btns button {
+  margin-bottom: 10px;
 }
 </style>
